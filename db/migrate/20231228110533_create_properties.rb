9ce8bc37c9d5a1af4e4bcec50c0d3f886c1021a9
yuvraj <yuvraj.chouhan@codiatic.com>
2023-12-31 00:11:27 +0530

    Property booking and listings updated'

--- db/migrate/20231228110533_create_properties.rb
+++ db/migrate/20231228110533_create_properties.rb
@@ -1,16 +1,19 @@
 class CreateProperties < ActiveRecord::Migration[7.1]
   def change
     create_table :properties do |t|
-      t.integer :registraion_no
+      t.string :property_name
+      t.string :property_about
       t.string :state
       t.string :city
-      t.string :country
       t.integer :zipcode
       t.string :property_type
-      t.string :property_sub_type
+      t.string :sub_property_type
       t.string :listed_for
-      t.string :size
+      t.string :property_size
       t.string :status
+      t.text :address
+      t.integer :price
+      t.boolean :verified, default: false
 
       t.timestamps
     end
