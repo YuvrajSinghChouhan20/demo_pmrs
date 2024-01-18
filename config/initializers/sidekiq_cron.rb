df08060a68a0216f4a9395606a66b768ddeaf387
yuvraj <yuvraj.chouhan@codiatic.com>
2024-01-18 23:23:34 +0530

    patch update

--- config/initializers/sidekiq_cron.rb
+++ config/initializers/sidekiq_cron.rb
@@ -3,3 +3,8 @@ Sidekiq::Cron::Job.create(
   cron: '0 0 0 * * *',
   class: 'GenerateMonthlyRentJob'
 )
+
+# Use the provided Redis URL from Render
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV['REDIS_URL'], size: 12, network_timeout: 5 }
+end
